c3902055165deba9f04950d19b2032169aee4ce0
prazit <prazit@gmail.com>
2022-12-29 18:25:23 +0700

    Now can save preset and can rotate in 3 axises.

--- demo/demo-util.js
+++ demo/demo-util.js
@@ -139,6 +139,8 @@ Appanel({
          * Often Use
          *
          * :: clock-1.html ::
+         * Appanel.util.seeNMove('.move-me','attr','x','y');
+         * Appanel.util.seeNMove('.move-me','css','left','top');
          * Appanel.util.seeNMove(Appanel.clock.backgroundAttributes,'member',['clockXInPercent','clockYInPercent','clockWInPercent','clockHInPercent','clockDegree'],0.001);
          * Appanel.util.seeNMove(Appanel.clock.backgroundAttributes,'member',['relativeX','relativeY'],1);
          * Appanel.util.seeNMove($('.cover'),'css',['left','top','width','height'],1);
